2d2e027ac6914ca27453913b73e8cc0b8ba2e3bc
Abigail-Salem-Tendo <a.tendo@alustudent.com>
2026-09-11 08:16:48 -0700

    document the module not function

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Module contains a function to transpose a matrix"""
 def matrix_transpose(matrix):
     """Return the transpose of a rectangular matrix"""
     return [[matrix[row][col] for row in range(len(matrix))]
--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Module has a funtion that does element addition"""
 def add_arrays(arr1, arr2):
     """Return element addition or none when lengths differ"""
     if len(arr1) != len(arr2):
