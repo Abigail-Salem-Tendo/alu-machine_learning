42de8f10443ab19f784d388327cea291b6f417eb
Abigail-Salem-Tendo <a.tendo@alustudent.com>
2026-09-12 05:50:08 -0700

    add matrix operations and slicing

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module has a fucntion that concatenates 2 lists"""
+
+
+def cat_arrays(arr1, arr2):
+    """This is a fucntion that concatenates two lists"""
+    return arr1 + arr2
---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""This module concatenaates two matrices on the same axis"""
+
+
+def cat_matrices2D(mat1, mat2, axis=o):
+    """This function concatenates matrices"""
+    if axis == 0:
+        # check if the columns match
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+
+        # create a new matrix
+        return [row[:] for row in mat1] + [row[:] for row in mat2]
+
+    elif axis == 1:
+        # the number of rows must match
+        if len(mat1) != len(mat2):
+            return None
+
+        # create a new matrix
+        result = []
+
+        for i in range(len(mat1)):
+            result.append(mat1[i][:] + mat2[i][:])
+        return result
+    return None
