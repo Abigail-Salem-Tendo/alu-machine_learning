1e657a4815265d119dd52d3750763c77d9684a93
Abigail-Salem-Tendo <a.tendo@alustudent.com>
2026-09-12 05:48:19 -0700

    5-across_the_planes.py

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""This module adds two matrices"""
+
+
+def add_matrices2D(mat1, mat2):
+    if len(mat1) != len(mat2):
+        return None
+
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+
+    result = []
+
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[i])):
+            row.append(mat1[i][j] + mat2[i][j])
+        result.append(row)
+
+    return result
