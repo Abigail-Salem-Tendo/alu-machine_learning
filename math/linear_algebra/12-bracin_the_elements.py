a9b2aef76aea2b792b01d0928926634dae5d3a3c
Abigail-Salem-Tendo <a.tendo@alustudent.com>
2026-09-12 07:12:56 -0700

    not importing a module

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-"""Perform element-wise arithmetic operations on NumPy arrays."""
-
-import numpy as np
+"""Perform element-wise operations on matrices."""
 
 
 def np_elementwise(mat1, mat2):
-    """Return the element-wise sum, difference, product, and quotient."""
-    mat1 = np.array(mat1)
-    mat2 = np.array(mat2)
+    """Return the sum, difference, product, and quotient of two matrices."""
     return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
